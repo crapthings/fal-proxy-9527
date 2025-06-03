b7b33b0c2edd0b4583c8de523296eb1fc42ea284
crapthings <crapthings@gmail.com>
2025-06-03 12:20:34 +0800

    update

--- src/pages/api/fal/webhook.js
+++ src/pages/api/fal/webhook.js
@@ -1,8 +1,7 @@
 export default async function handler (req, res) {
   const { body } = req
-  const { event, data } = body
 
-  console.log(body)
+  console.log('fal proxy webhook', body)
 
   res.status(200).json({ message: 'Webhook received' })
 }
