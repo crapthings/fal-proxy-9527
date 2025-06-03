0f0b55f1d7488866ea0d1320c2441a0b4d856eec
crapthings <crapthings@gmail.com>
2025-06-03 19:42:15 +0800

    update

--- src/pages/api/fal/proxy.js
+++ src/pages/api/fal/proxy.js
@@ -1,13 +1,18 @@
 import { fal } from '@fal-ai/client'
 
 export default async function handler (req, res) {
-  const { model } = req.query
-  const { body } = req
+  try {
+    const { model } = req.query
+    const { body } = req
 
-  console.log('model', model)
-  console.log('body', body)
+    console.log('model', model)
+    console.log('body', body)
 
-  const resp = await fal.queue.submit(model, body)
+    const resp = await fal.queue.submit(model, body)
 
-  res.status(200).json(resp)
+    res.status(200).json(resp)
+  } catch (error) {
+    console.error(error)
+    res.status(500).json({ error: error.message })
+  }
 }
