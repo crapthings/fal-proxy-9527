b21b103ce7dfa1f0e5bd0c1ec1923ced64aca9e7
crapthings <crapthings@gmail.com>
2025-06-03 15:10:21 +0800

    update

--- src/pages/api/fal/proxy.js
+++ src/pages/api/fal/proxy.js
@@ -1,1 +1,13 @@
-export { handler as default } from '@fal-ai/server-proxy/nextjs'
+import * as fal from '@fal-ai/serverless-client'
+
+export default async function handler (req, res) {
+  const { model } = req.query
+  const { body } = req
+
+  console.log('model', model)
+  console.log('body', body)
+
+  const resp = await fal.queue.submit(model, body)
+
+  res.status(200).json(resp)
+}
